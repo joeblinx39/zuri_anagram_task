f5bb688b0fb7e0fa4c146f35a6eaf0d192f0b108
Joseph Bassey John <60972495+joeblinx39@users.noreply.github.com>
2022-05-26 17:51:38 +0100

    Created an anagram finder python project

---
+++ anagram.py
@@ -0,0 +1,19 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+def find_anagram(word, anagram):
+    #checks if the length of words are the same and sorts them
+    word = input("Enter word: ")
+    anagram = input("Enter anagram: ")
+    if(len(word) == len(anagram)):
+        sorted_word = sorted(word)
+        sorted_anagram = sorted(anagram)
+        #checks if the sorted words are the same
+        if(sorted_anagram == sorted_word):
+            return True
+        else:
+            return False
+    else:
+            return False
+print(find_anagram('sweat', 'weats'))
